74df84724e92474c544b717625e25073791b8165
Ming <c2083117697@qq.com>
2022-06-26 11:06:41 +0800

    完成了路由、layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Layout from '@/views/Layout'
+import Home from '@/views/Home'
+import My from '@/views/My'
+import Questron from '@/views/Questron'
+import Video from '@/views/Video'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      { path: 'home', component: Home },
+      { path: 'video', component: Video },
+      { path: 'questron', component: Questron },
+      { path: 'my', component: My }
+    ]
+  }
+
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <router-view />
+    <van-tabbar route>
+      <van-tabbar-item replace to="/home" icon="home-o">
+        <i class="toutiao toutiao-shouye" slot="icon"></i>
+        首页
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/video" icon="search">
+        <i class="toutiao toutiao-shipin" slot="icon"></i>
+        视频
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/questron" icon="search">
+        <i class="toutiao toutiao-wenda" slot="icon"></i>
+        问答
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/my" icon="search">
+        <i class="toutiao toutiao-wode" slot="icon"></i>
+        我的
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  created () { },
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
+  components: {}
+}
+</script>
+
+<style scoped lang='less'>
+.toutiao {
+  font-size: 40px;
+}
+.van-tabbar-item__text {
+  font-size: 20px !important;
+}
+</style>
